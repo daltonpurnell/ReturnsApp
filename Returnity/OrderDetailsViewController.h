45bed75a585173b910011d05abbe249118ea586b
daltonpurnell <daltonpurnell@live.com>
2015-06-29 11:29:42 -0600

    adds order details view

---
+++ Returnity/OrderDetailsViewController.h
@@ -0,0 +1,20 @@
+//
+//  OrderDetailsViewController.h
+//  Returnity
+//
+//  Created by Dalton on 6/29/15.
+//  Copyright (c) 2015 Dalton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OrderDetailsViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *priceLabel;
+@property (weak, nonatomic) IBOutlet UILabel *pickupLocationLabel;
+@property (weak, nonatomic) IBOutlet UILabel *pickupDeadlineLabel;
+@property (weak, nonatomic) IBOutlet UILabel *returnLocationLabel;
+@property (weak, nonatomic) IBOutlet UILabel *returnDeadlineLabel;
+@property (weak, nonatomic) IBOutlet UITextField *emailTextField;
+
+
+@end
