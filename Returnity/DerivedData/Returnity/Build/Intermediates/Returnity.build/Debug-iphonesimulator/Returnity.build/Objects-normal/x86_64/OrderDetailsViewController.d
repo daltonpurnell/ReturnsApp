7947c452a4c198d73aef5c45f4dfd70ac0c69e2b
daltonpurnell <daltonpurnell@live.com>
2015-06-29 11:33:56 -0600

    small updates to constraints

---
+++ Returnity/DerivedData/Returnity/Build/Intermediates/Returnity.build/Debug-iphonesimulator/Returnity.build/Objects-normal/x86_64/OrderDetailsViewController.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/djp1989/Documents/Development/PersonalProjects/ReturnsApp/Returnity/OrderDetailsViewController.m \
+  /Users/djp1989/Documents/Development/PersonalProjects/ReturnsApp/Returnity/OrderDetailsViewController.h
